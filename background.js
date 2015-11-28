2f31a750cd7222110f6511fb79b0b4e96857d500
Cole Wagner <colelc150@gmail.com>
2015-11-28 18:59:36 -0500

    Clipboard uploading complete

--- background.js
+++ background.js
@@ -1,4 +1,3 @@
-//var deletes = [];
 var bdgclr = [0, 255, 0, 160];
 chrome.browserAction.onClicked.addListener(function(tab) {
     var x = document.createElement("a");
@@ -14,30 +13,62 @@ chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.browserAction.setBadgeBackgroundColor({color: bdgclr, tabId: tab.id});
 });
 
+save();
 setInterval(save, 60000); //one minute interval for saving/pushing the history
 
 function save() {
-    //get the last start time and then update the last start time
-    var lastStart = 0;
+    //get the last start time
     chrome.storage.local.get('lastStartUp', function(result){
-        if(result)
+        var lastStart = 0;
+        if(result.lastStartUp)
         {
-            lastStart = result;
+            lastStart = result.lastStartUp;
         }
-    })
-    chrome.storage.local.set({'lastStartUp': (new Date()).getTime()});
+        alert(lastStart);
+        saveLastStartUpCallback(lastStart);
+    });
+}
 
+function saveLastStartUpCallback(lastStart){
     //populate logDatas with history data for history items that are new since the last start time
-    var logDatas = [];
-    chrome.history.search({text: '', startTime = lastStart, endTime = (new Date()).getTime(), maxResults: 10000}, function(histItems){
+    chrome.history.search({text: '', startTime: lastStart, endTime: (new Date()).getTime(), maxResults: 10000}, function(histItems){
+        var logDatas = [];
         for(i = 0; i < histItems.length; i++)
         {
             var h = histItems[i];
-            logDatas.push({date: h.lastVisitTime, url: h.url, loggedInputs: {visitCount: h.visitCount, typedCount: h.typedCount} });
+            var data = new Object();
+            var subData = new Object();
+            subData.visitCount = h.visitCount;
+            subData.typedCount = h.typedCount;
+
+            data.date = h.lastVisitTime;
+            data.url = h.url;
+            data.loggedInputs = subData;
+
+            logDatas.push(data);
+        }
+        if (logDatas.length > 0){
+            saveSearchCallback(logDatas);
+        }else
+        {
+            //no new history so increment checkpoint
+            chrome.storage.local.set({'lastStartUp': (new Date()).getTime()});
         }
     });
-    var logMessJSON = JSON.stringify({logs: logDatas});
+}
 
-    //now push the JSON to the other extension. 
+function saveSearchCallback(logDatas) {
+    var logMess = {logs: logDatas};
+    var logMessJSON = JSON.stringify(logMess);
+
+    //now push the JSON to the other extension. (using clipboard)
+    var txtbox = document.getElementById('txtbox');
+    txtbox.value = logMessJSON;
+    txtbox.select();
+    if (document.execCommand('copy'))
+    {
+        //copy was successful so update the lastStartUp time to checkpoint history upload.
+        chrome.storage.local.set({'lastStartUp': (new Date()).getTime()});
+    }
 }
 
